6423e94bed7a4b8e2332d2dfa210549b49d1cf0c
superukie <superukie@naver.com>
2024-02-04 00:40:53 +0900

    Initial commit (Swagger OAS3.0 settings)

---
+++ src/main/java/com/longvacation/projectlongvacation/ProjectLongVacationApplication.java
@@ -0,0 +1,13 @@
+package com.longvacation.projectlongvacation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLongVacationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLongVacationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/longvacation/projectlongvacation/ProjectLongVacationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longvacation.projectlongvacation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLongVacationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
